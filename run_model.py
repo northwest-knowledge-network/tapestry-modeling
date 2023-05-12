8ae38f8e9e64fc9ade8a15e7f4e9a71823c1c519
tvarrelman <tannervarrelman@gmail.com>
2023-05-12 14:58:33 -0700

    non-working multiprocessing implementation

--- run_model.py
+++ run_model.py
@@ -18,9 +18,17 @@ from gravity_trade import GravityModel
 import argparse
 from dotenv import load_dotenv
 import os
+from concurrent.futures import ProcessPoolExecutor as Pool
 
+# load environment variables
 load_dotenv()
 
+# define command line arguments
+parser = argparse.ArgumentParser(description='This program pulls an impedance matrix and supply/demand data from a postgres database, and runs the gravity model on the data. The output is a matrix of shipments between trade palces.')
+parser.add_argument('-impedance', help="The name of the impedance that you wish to use as input to the gravity model. This parameter is required.", required=True)
+parser.add_argument('-trade_commodity_id', help="The trade commodity index that you wish to calculate. If this parameter is not specified, the program will be ran for every commodity", required=False)
+
+# establish connection to database
 def create_connection():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
@@ -31,6 +39,7 @@ def create_connection():
     )
     return conn
 
+# get impedance matrix from database
 def get_impedance_matrix(cursor, impedance_var):
     query = sql.SQL("SELECT fromplaceid, toplaceid, {} FROM us2012.fact_impedances_4135;")
     cursor.execute(query.format(sql.Identifier(impedance_var)))
@@ -42,29 +51,57 @@ def get_impedance_matrix(cursor, impedance_var):
     matrix = df.to_numpy()
     return matrix
 
+# get supply and demand data from database
 def get_supply_demand(cursor, commodity_id):
     query = sql.SQL("SELECT tradeplace_id, supply_amount, demand_amount FROM us2012.fact_masses_409x4135 WHERE trade_commodity_id = {};")
     cursor.execute(query.format(sql.Literal(commodity_id)))
     supp_demand_df = pd.DataFrame(cur.fetchall(), columns=['tradeplace_id', 'supply_amount', 'demand_amount'])
     return supp_demand_df
 
-if __name__ == '__main__':
-    conn = create_connection()
-    cur = conn.cursor()
-    imp_mat = get_impedance_matrix(cur, 'hwyimpedence')
-    supp_demand_df = get_supply_demand(cur, 5030)
-    conn.close()
-    conn = None
-    if len(supp_demand_df) != imp_mat.shape[0] or len(supp_demand_df) != imp_mat.shape[1]:
+# get list of trade commodity ids
+def get_trade_commodity_id(cursor):
+    query = sql.SQL("SELECT DISTINCT trade_commodity_id FROM us2012.fact_masses_409x4135;")
+    cursor.execute(query)
+    trade_commodity_id = pd.DataFrame(cur.fetchall(), columns=['trade_commodity_id'])['trade_commodity_id'].unique().tolist()
+    return trade_commodity_id
+
+# run the gravity model
+def main(trade_id):
+    supp_demand_df = get_supply_demand(cur, trade_id)
+    if len(supp_demand_df) != imp_matrix.shape[0] or len(supp_demand_df) != imp_matrix.shape[1]:
         print('ERROR: SUPPLY DEMAND AND IMPEDANCE MATRIX NOT SAME SIZE')
-        exit()
-    gravity_model = GravityModel(imp_mat, supp_demand_df)
+        print(len(supp_demand_df))
+        print(imp_matrix.shape)
+        return
+
+    gravity_model = GravityModel(imp_matrix, supp_demand_df)
     gravity_model.format_data()
     gravity_model.calculate_cost_matrix()
     gravity_model.calculate_attraction_matrices()
     gravity_model.calculate_A_and_B()
     gravity_model.calculate_prob_matrix()
     gravity_model.calculate_shipping_matrix()
+    print()
     print('TOTAL SHIPPED SUPPLY: ', gravity_model.total_shipped_supply)
     print('TOTAL SHIPPED DEMAND: ', gravity_model.total_shipped_demand)
-    pd.DataFrame(data=gravity_model.S).to_csv('S_shipments_trip_matrix_v2.csv')
+    pd.DataFrame(data=gravity_model.S).to_csv('/Users/tvarrelman/Documents/gravity_model/example_data/example_output/S_shipments_trip_matrix_{0}.csv'.format(trade_id))
+    conn.close()
+
+if __name__ == '__main__':
+    # get command line arguments
+    args = parser.parse_args()
+    impedance_var = args.impedance
+    conn = create_connection()
+    cur = conn.cursor()
+    global imp_matrix
+    imp_matrix = get_impedance_matrix(cur, impedance_var)
+    #trade_list = get_trade_commodity_id(cur)
+    #trade_list = [5030]
+    trade_list = [5041, 5348, 5134, 5120]
+    #print(imp_matrix.shape)
+    with Pool(max_workers = 4) as pool:
+        pool.map(main, trade_list)
+    #for trade_id in trade_list:
+    #    main(trade_id)
+
+    conn.close()
