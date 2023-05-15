888e208f7463186a114c5b63df7dc8245fa338e1
tvarrelman <tannervarrelman@gmail.com>
2023-05-15 11:30:31 -0700

    modified for ORM, and working multi-threading

--- run_model.py
+++ run_model.py
@@ -4,72 +4,89 @@
 
 @author: Tanner Varrelman tvarrelman@uidaho.edu
 
-This code pulls an impedance matrix and supply/demand data from a postgres database,
+This code pulls an impedence matrix and supply/demand data from a postgres database,
 and runs the gravity model on the data. The output is a matrix of shipments between
 trade places.
 
 """
 
-import psycopg2
 from psycopg2 import sql
-import numpy as np
 import pandas as pd
 from gravity_trade import GravityModel
 import argparse
 from dotenv import load_dotenv
 import os
-from concurrent.futures import ProcessPoolExecutor as Pool
+from concurrent.futures import ThreadPoolExecutor as Pool
+#from multiprocessing import Pool
+from models import Impedances, Masses
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from itertools import repeat
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm.session import close_all_sessions
+import time 
+import numpy as np
 
 # load environment variables
 load_dotenv()
 
+db_info = os.getenv('DATABASE_URI')
+engine = create_engine(db_info, echo=False)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
 # define command line arguments
-parser = argparse.ArgumentParser(description='This program pulls an impedance matrix and supply/demand data from a postgres database, and runs the gravity model on the data. The output is a matrix of shipments between trade palces.')
-parser.add_argument('-impedance', help="The name of the impedance that you wish to use as input to the gravity model. This parameter is required.", required=True)
+parser = argparse.ArgumentParser(description='This program pulls an impedence matrix and supply/demand data from a postgres database, and runs the gravity model on the data. The output is a matrix of shipments between trade palces.')
+parser.add_argument('-impedence', help="The name of the impedence that you wish to use as input to the gravity model. This parameter is required.", required=True)
 parser.add_argument('-trade_commodity_id', help="The trade commodity index that you wish to calculate. If this parameter is not specified, the program will be ran for every commodity", required=False)
 
-# establish connection to database
-def create_connection():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port = os.getenv("DB_PORT"),
-        database=os.getenv("DB"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS")
-    )
-    return conn
+impedence_vars = {
+    'hwyimpedence': Impedances.hwyimpedence,
+    'rrimpedence': Impedances.rrimpedence,
+    'waterimpedence': Impedances.waterimpedence,
+    'comboimpedence': Impedances.comboimpedence
+}
 
-# get impedance matrix from database
-def get_impedance_matrix(cursor, impedance_var):
-    query = sql.SQL("SELECT fromplaceid, toplaceid, {} FROM us2012.fact_impedances_4135;")
-    cursor.execute(query.format(sql.Identifier(impedance_var)))
-    df = pd.DataFrame(cur.fetchall(), columns=['fromplaceid', 'toplaceid', impedance_var])
+# get impedence matrix from database
+def get_impedence_matrix(impedence_var):
+    session = Session()
+    impedence = impedence_vars.get(impedence_var)
+    if not impedence:
+        print('ERROR: INVALID IMPEDENCE VARIABLE')
+        exit()
+    query = session.query(Impedances.fromplaceid, Impedances.toplaceid, impedence).all()
+    df = pd.DataFrame(query, columns=['fromplaceid', 'toplaceid', impedence_var])
     if len(df['fromplaceid'].unique()) != len(df['toplaceid'].unique()):
         print('ERROR: FIPS MATRIX NOT SQUARE')
         exit()
-    df = df.pivot_table(values='hwyimpedence', index='fromplaceid', columns='toplaceid')
+    df = df.pivot_table(values=impedence_var, index='fromplaceid', columns='toplaceid')
     matrix = df.to_numpy()
+    print('finished matrix')
+    session.close()
     return matrix
 
 # get supply and demand data from database
-def get_supply_demand(cursor, commodity_id):
-    query = sql.SQL("SELECT tradeplace_id, supply_amount, demand_amount FROM us2012.fact_masses_409x4135 WHERE trade_commodity_id = {};")
-    cursor.execute(query.format(sql.Literal(commodity_id)))
-    supp_demand_df = pd.DataFrame(cur.fetchall(), columns=['tradeplace_id', 'supply_amount', 'demand_amount'])
+def get_supply_demand(commodity_id):
+    session = Session()
+    query = session.query(Masses.tradeplace_id, Masses.supply_amount, Masses.demand_amount).filter(Masses.trade_commodity_id == commodity_id).all()
+    supp_demand_df = pd.DataFrame(query, columns=['tradeplace_id', 'supply_amount', 'demand_amount'])
+    session.close()
     return supp_demand_df
 
 # get list of trade commodity ids
-def get_trade_commodity_id(cursor):
-    query = sql.SQL("SELECT DISTINCT trade_commodity_id FROM us2012.fact_masses_409x4135;")
-    cursor.execute(query)
-    trade_commodity_id = pd.DataFrame(cur.fetchall(), columns=['trade_commodity_id'])['trade_commodity_id'].unique().tolist()
+def get_trade_commodity_id():
+    session = Session()
+    query = session.query(Masses.trade_commodity_id).distinct().all()
+    trade_commodity_id = pd.DataFrame(query, columns=['trade_commodity_id'])['trade_commodity_id'].unique().tolist()
+    session.close()
     return trade_commodity_id
 
 # run the gravity model
-def main(trade_id):
-    supp_demand_df = get_supply_demand(cur, trade_id)
+def main(imp_matrix, trade_id):
+    start_time = time.time()
+    supp_demand_df = get_supply_demand(trade_id)
     if len(supp_demand_df) != imp_matrix.shape[0] or len(supp_demand_df) != imp_matrix.shape[1]:
-        print('ERROR: SUPPLY DEMAND AND IMPEDANCE MATRIX NOT SAME SIZE')
+        print('ERROR: SUPPLY DEMAND AND IMPEDENCE MATRIX NOT SAME SIZE')
         print(len(supp_demand_df))
         print(imp_matrix.shape)
         return
@@ -81,27 +98,30 @@ def main(trade_id):
     gravity_model.calculate_A_and_B()
     gravity_model.calculate_prob_matrix()
     gravity_model.calculate_shipping_matrix()
-    print()
-    print('TOTAL SHIPPED SUPPLY: ', gravity_model.total_shipped_supply)
-    print('TOTAL SHIPPED DEMAND: ', gravity_model.total_shipped_demand)
+    #print()
+    #print('TOTAL SHIPPED SUPPLY: ', gravity_model.total_shipped_supply)
+    #print('TOTAL SHIPPED DEMAND: ', gravity_model.total_shipped_demand)
+    if abs(gravity_model.total_shipped_supply - gravity_model.total_shipped_demand) > 1:
+        print('ERROR: SUPPLY AND DEMAND NOT BALANCED FOR: TRADE ID {0}'.format(trade_id))
     pd.DataFrame(data=gravity_model.S).to_csv('/Users/tvarrelman/Documents/gravity_model/example_data/example_output/S_shipments_trip_matrix_{0}.csv'.format(trade_id))
-    conn.close()
+    end_time = time.time()
+    print('Runtime: ', end_time - start_time, ' seconds')
 
 if __name__ == '__main__':
     # get command line arguments
     args = parser.parse_args()
-    impedance_var = args.impedance
-    conn = create_connection()
-    cur = conn.cursor()
-    global imp_matrix
-    imp_matrix = get_impedance_matrix(cur, impedance_var)
-    #trade_list = get_trade_commodity_id(cur)
-    #trade_list = [5030]
-    trade_list = [5041, 5348, 5134, 5120]
-    #print(imp_matrix.shape)
-    with Pool(max_workers = 4) as pool:
-        pool.map(main, trade_list)
-    #for trade_id in trade_list:
-    #    main(trade_id)
+    impedence_var = args.impedence
+    start_time = time.time()
+    imp_matrix = get_impedence_matrix(impedence_var)
+    end_time = time.time()
+    print('Matrix time: ', end_time - start_time, ' seconds')
+    trade_list = get_trade_commodity_id()
+    #trade_list = [5041, 5348, 5134, 5120]
+    with Pool(2) as pool:
+        results = list(pool.map(main, repeat(imp_matrix), trade_list))
+    average_run_time = np.mean(results)
+    print(average_run_time)
+    close_all_sessions()
 
-    conn.close()
+
+  
