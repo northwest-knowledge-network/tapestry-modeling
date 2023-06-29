ea726aa7ec1ad3d4d60a4ad8d64ab2947e087e82
Tanner Varrelman <tannervarrelman@gmail.com>
2023-06-29 15:33:48 -0700

    added data processing/cleaned code

--- QCEW_to_BEA/conversion.py
+++ QCEW_to_BEA/conversion.py
@@ -36,40 +36,40 @@ class QCEWtoBEA:
             bridge_data = db.session.query(*bridge_cols).filter(NAICStoBEA.bls_own_code == own_code).all()
             bridge_df = pd.DataFrame.from_records(bridge_data, columns=['r_seq', 'c_seq', 'r_naics6_code', 'c_bea_code_2_lvl_6', 'naics_to_io_coeffs', 'oen_code'])
             csv_data = bea_bridge(req_data, wage_info, emp_info, bridge_df, csv_data)
-        self.
+        self.csv_data = csv_data
 
-def bea_bridge(req_data, wage_info, emp_info, bridge_df, csv_data):
-    bea = pd.read_csv('./app/static/files/bea_cols.csv', dtype=str)
-    naics = pd.read_csv('./app/static/files/naics_rows.csv', dtype=str)
-    if naics['naics >>'].isnull().values.any():
-        naics = naics.dropna(subset=['naics >>'])
-    naics['naics >>'] = naics['naics >>'].astype(int)
-    if bea['column_index'].isnull().values.any():
-        bea = bea.dropna(subset=['column_index'])
-    bea['column_index'] = bea['column_index'].astype(int)
-    sectors = pd.read_csv('./app/static/files/BEA_sectors.csv')
+    def bea_bridge(req_data, wage_info, emp_info, bridge_df, csv_data):
+        bea = pd.read_csv('./app/static/files/bea_cols.csv', dtype=str)
+        naics = pd.read_csv('./app/static/files/naics_rows.csv', dtype=str)
+        if naics['naics >>'].isnull().values.any():
+            naics = naics.dropna(subset=['naics >>'])
+        naics['naics >>'] = naics['naics >>'].astype(int)
+        if bea['column_index'].isnull().values.any():
+            bea = bea.dropna(subset=['column_index'])
+        bea['column_index'] = bea['column_index'].astype(int)
+        sectors = pd.read_csv('./app/static/files/BEA_sectors.csv')
 
-    csv_data = csv_data.merge(naics, left_on='naics_code', right_on='naics6_code')
-    csv_data = csv_data.merge(bridge_df, left_on='naics >>', right_on='r_seq', suffixes=('', '_y'))
-    csv_data = csv_data.merge(bea, left_on='c_seq', right_on='column_index')
-    csv_data['own_code'] = csv_data['own_code'].astype(int)
-    csv_data['tap_estabs_count'] = csv_data['tap_estabs_count'].astype(int)
-    csv_data[wage_info[1]] = csv_data[wage_info[1]].astype(int)
-    csv_data[emp_info[1]] = csv_data[emp_info[1]].astype(int)
-    csv_data['naics_to_io_coeffs'] = csv_data['naics_to_io_coeffs'].astype(float)
+        csv_data = csv_data.merge(naics, left_on='naics_code', right_on='naics6_code')
+        csv_data = csv_data.merge(bridge_df, left_on='naics >>', right_on='r_seq', suffixes=('', '_y'))
+        csv_data = csv_data.merge(bea, left_on='c_seq', right_on='column_index')
+        csv_data['own_code'] = csv_data['own_code'].astype(int)
+        csv_data['tap_estabs_count'] = csv_data['tap_estabs_count'].astype(int)
+        csv_data[wage_info[1]] = csv_data[wage_info[1]].astype(int)
+        csv_data[emp_info[1]] = csv_data[emp_info[1]].astype(int)
+        csv_data['naics_to_io_coeffs'] = csv_data['naics_to_io_coeffs'].astype(float)
 
-    csv_data[wage_info[1] + '_bea'] = np.array(csv_data[wage_info[1]].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
-    csv_data[emp_info[1] + '_bea'] = np.array(csv_data[emp_info[1]].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
-    csv_data['tap_estabs_count_bea'] = np.array(csv_data['tap_estabs_count'].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
-    csv_data = csv_data.drop(['naics >>', 'column_index', 'r_seq', 'c_seq', 'naics_to_io_coeffs', wage_info[1], emp_info[1], 'tap_estabs_count', 'naics_code', 'naics6_code'], axis=1)
-    if req_data.get('fips_calc') == 'fipsAgg':
-        csv_data = csv_data.groupby(['bea_code', 'own_code', 'year'])[["tap_estabs_count_bea", wage_info[1] + '_bea', emp_info[1] + '_bea']].sum().reset_index()
-    else:
-        csv_data = csv_data.groupby(['area_fips', 'bea_code', 'own_code', 'year'])[["tap_estabs_count_bea", wage_info[1] + '_bea', emp_info[1] + '_bea']].sum().reset_index()
-    csv_data['bea_num'] = [x.split('_')[1] for x in csv_data['bea_code'].tolist()]
-    csv_data = csv_data.merge(sectors, left_on="bea_num", right_on="Num")
-    csv_data.pop('Num')
-    csv_data.insert(0, 'bea_num', csv_data.pop('bea_num'))
-    csv_data.insert(1, 'bea_name', csv_data.pop('Name'))
-    csv_data.insert(2, 'bea_code', csv_data.pop('bea_code'))
-    return csv_data
+        csv_data[wage_info[1] + '_bea'] = np.array(csv_data[wage_info[1]].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
+        csv_data[emp_info[1] + '_bea'] = np.array(csv_data[emp_info[1]].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
+        csv_data['tap_estabs_count_bea'] = np.array(csv_data['tap_estabs_count'].tolist()) * np.array(csv_data['naics_to_io_coeffs'].tolist())
+        csv_data = csv_data.drop(['naics >>', 'column_index', 'r_seq', 'c_seq', 'naics_to_io_coeffs', wage_info[1], emp_info[1], 'tap_estabs_count', 'naics_code', 'naics6_code'], axis=1)
+        if req_data.get('fips_calc') == 'fipsAgg':
+            csv_data = csv_data.groupby(['bea_code', 'own_code', 'year'])[["tap_estabs_count_bea", wage_info[1] + '_bea', emp_info[1] + '_bea']].sum().reset_index()
+        else:
+            csv_data = csv_data.groupby(['area_fips', 'bea_code', 'own_code', 'year'])[["tap_estabs_count_bea", wage_info[1] + '_bea', emp_info[1] + '_bea']].sum().reset_index()
+        csv_data['bea_num'] = [x.split('_')[1] for x in csv_data['bea_code'].tolist()]
+        csv_data = csv_data.merge(sectors, left_on="bea_num", right_on="Num")
+        csv_data.pop('Num')
+        csv_data.insert(0, 'bea_num', csv_data.pop('bea_num'))
+        csv_data.insert(1, 'bea_name', csv_data.pop('Name'))
+        csv_data.insert(2, 'bea_code', csv_data.pop('bea_code'))
+        return csv_data
--- QCEW_to_BEA/run_conversion.py
+++ QCEW_to_BEA/run_conversion.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import os 
 import sys 
 import argparse
+import pandas as pd
+from itertools import product
+# models are in the parent directory, so we need to adjust the path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
@@ -11,17 +14,20 @@ from models import QCEWComp, NAICStoBEA
 
 # load environment variables
 load_dotenv()
+
 # initialize database connection
 db_info = os.getenv('DATABASE_URI2')
 engine = create_engine(db_info, echo=False)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
+
 # define command line arguments
 parser = argparse.ArgumentParser(description='This program is used to convert QCEW data from 6 digit NAICS codes to BEA sectors.')
 parser.add_argument('-ownership', help="The QCEW ownership code is required to run the program. Allowed values include 0, 1, 2, 3, 5.", required=True)
 parser.add_argument('-year', help="A year corresponding to QCEW data must be provided. Currently supported years include 2001-2021", required=True)
 parser.add_argument('-employer_wage', help="The employer wage number that you wish to convert. Allowed values include 1, 2, 3, 4, 5.", required=True)
 
+# given employer wage number, create field list
 def create_field_list(emp_wage_number):
     # base field list
     field_list = [
@@ -50,12 +56,37 @@ def create_field_list(emp_wage_number):
     emp_info = emp_dict.get(int(emp_wage_number))
     field_list.append(wage_info[0])
     field_list.append(emp_info[0])
-    return field_list
+    return field_list, wage_info[1], emp_info[1]
+
+# given a list of tuples, find outliers
+def find_outliers(list1, list2):
+    t_length = len(list2[0])
+    set2 = {tuple(t[:4]) for t in list2}
+    outliers = []
+    for tuple1 in list1:
+        if tuple(tuple1[:4]) not in set2:
+            outlier = tuple1 + (0,) * (t_length - 4)  # Add zeros to make same length as tuple1
+            outliers.append(outlier)
+    return outliers
+
+# given user provided arguments, pull the QCEW data, generate all possible value combinations, and fill missing combinations with zeros
+def get_QCEW(args):
+    session = Session()
+    field_list, wage_col_name, emp_col_name = create_field_list(args.employer_wage)
+    query = session.query(*field_list).filter(QCEWComp.year == args.year).filter(QCEWComp.own_code == args.ownership).all()
+    csv_data = pd.DataFrame.from_records(query, columns=['area_fips', 'own_code', 'naics_code', 'year', 'tap_estabs_count', wage_col_name, emp_col_name])
+    fips = csv_data.area_fips.unique()
+    naics = csv_data.naics_code.unique()
+    ownership = csv_data.own_code.unique()
+    year = csv_data.year.unique()
+    csv_data = None
+    param_combos = list(product(fips, ownership, naics, year))
+    outliers = find_outliers(param_combos, query)
+    out_data = query + outliers
+    csv_data = pd.DataFrame(out_data, columns=['area_fips', 'own_code', 'naics_code', 'year', 'tap_estabs_count', wage_col_name, emp_col_name])
+    print(len(csv_data) == len(param_combos))
 
 if __name__ == '__main__':
     session = Session()
     args = parser.parse_args()
-    emp_wage_num = args.employer_wage
-    field_list = create_field_list(emp_wage_num)
-    query = session.query(*field_list).filter(QCEWComp.year == args.year).first()
-    print(query)
+    get_QCEW(args)
