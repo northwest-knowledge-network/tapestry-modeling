0838345347f6366539bb9718cb6e92ae446d92e9
Tanner Varrelman <tannervarrelman@gmail.com>
2023-06-30 14:11:09 -0700

    added encoding

--- QCEW_to_BEA/conversion.py
+++ QCEW_to_BEA/conversion.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 from sqlalchemy import create_engine
--- QCEW_to_BEA/run_conversion.py
+++ QCEW_to_BEA/run_conversion.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
